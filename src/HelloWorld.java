cac4536faa929e7a169a0bfb695fab5d6b630ea6
Marlon Morris <mmorris@Marlons-MacBook-Pro.local>
2019-03-11 17:19:28 -0600

    adding new tests and updating gitignore:

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Poppy Grace!");
+        System.out.println("You are beautiful!");
+        System.out.println("and so smart like your mom!");
+    }
+}
